bff7d95950786aa1c1c847b8038bcccf0153a168
Alex Sandro <lexilva@gmail.com>
2024-12-04 23:02:58 -0300

    Estilização da Classe item

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,29 @@
 const emojis =[
-    
+    "🐵",
+    "🐵",
+    "🐶",
+    "🐶",
+    "🐨",
+    "🐨",
+    "🐷",
+    "🐷",
+    "🐴",
+    "🐴",
+    "🦄",
+    "🦄",
+    "🐸",
+    "🐸",
+    "🐔",
+    "🐔",
 ];
 let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(()=>(Math.random ()> 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className ="item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
